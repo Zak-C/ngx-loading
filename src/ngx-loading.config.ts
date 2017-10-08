d47ac9bf5d1a531eb0d90622c0912d74f32f9074
guilhermehtk <guilhermehtk@hotmail.com>
2017-10-08 16:27:20 -0300

    Add fading circle

--- src/ngx-loading.config.ts
+++ src/ngx-loading.config.ts
@@ -34,5 +34,6 @@ export const ANIMATION_TYPES = {
     threeBounce: 'three-bounce',
     rotatingPlane: 'rotating-plane',
     rectangleBounce: 'rectangle-bounce',
-    wanderingCubes: 'wandering-cubes'
+    wanderingCubes: 'wandering-cubes',
+    fadingCircle: 'fading-circle'
 }
