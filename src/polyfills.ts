c032039c53bc5a6f7bd8b081464795b2fcccfadc
Thomas James <thomasat12sky@live.co.uk>
2019-06-11 15:43:31 +0100

    Removed @angular/http and updated core-js to 3.1.3

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -43,7 +43,6 @@
 
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect';
 
 
 /**
@@ -71,9 +70,7 @@ import 'core-js/es7/reflect';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
