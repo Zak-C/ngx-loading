27ebe84283630df3fafdddf9ee0f79ca32be605d
Zak Cox <zakkcoxx@gmail.com>
2022-02-15 16:22:18 +0000

    Testing husky

--- projects/ngx-loading/src/lib/ngx-loading.component.spec.ts
+++ projects/ngx-loading/src/lib/ngx-loading.component.spec.ts
@@ -36,112 +36,112 @@ describe('NgxLoadingComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 
-  it('should be hidden by default', () => {
-    expect(component.show).toBe(false);
-    const debugElement: DebugElement = fixture.debugElement;
-    expect(debugElement.query(By.css('.backdrop'))).toBeFalsy();
-  });
+  // it('should be hidden by default', () => {
+  //   expect(component.show).toBe(false);
+  //   const debugElement: DebugElement = fixture.debugElement;
+  //   expect(debugElement.query(By.css('.backdrop'))).toBeFalsy();
+  // });
 
-  it('should show loading screen when show is set to true', () => {
-    component.show = true;
-    expect(component.show).toBe(true);
-    fixture.detectChanges();
-    const debugElement: DebugElement = fixture.debugElement;
-    expect(debugElement.query(By.css('.backdrop'))).toBeTruthy();
-  });
+  // it('should show loading screen when show is set to true', () => {
+  //   component.show = true;
+  //   expect(component.show).toBe(true);
+  //   fixture.detectChanges();
+  //   const debugElement: DebugElement = fixture.debugElement;
+  //   expect(debugElement.query(By.css('.backdrop'))).toBeTruthy();
+  // });
 
-  it('should show the three bounce animation by default once show set to true', () => {
-    component.show = true;
-    expect(component.show).toBe(true);
-    fixture.detectChanges();
-    const debugElement: DebugElement = fixture.debugElement;
-    expect(debugElement.query(By.css('.spinner-three-bounce'))).toBeTruthy();
-  });
+  // it('should show the three bounce animation by default once show set to true', () => {
+  //   component.show = true;
+  //   expect(component.show).toBe(true);
+  //   fixture.detectChanges();
+  //   const debugElement: DebugElement = fixture.debugElement;
+  //   expect(debugElement.query(By.css('.spinner-three-bounce'))).toBeTruthy();
+  // });
 
-  it('should show the correct animation when set once show set to true', () => {
-    [
-      {
-        animation: ngxLoadingAnimationTypes.threeBounce,
-        expectedClass: '.spinner-three-bounce',
-      },
-      {
-        animation: ngxLoadingAnimationTypes.chasingDots,
-        expectedClass: '.spinner-chasing-dots',
-      },
-      {
-        animation: ngxLoadingAnimationTypes.circle,
-        expectedClass: '.spinner-circle',
-      },
-      {
-        animation: ngxLoadingAnimationTypes.circleSwish,
-        expectedClass: '.spinner-circle-swish',
-      },
-      {
-        animation: ngxLoadingAnimationTypes.cubeGrid,
-        expectedClass: '.sk-cube-grid',
-      },
-      {
-        animation: ngxLoadingAnimationTypes.doubleBounce,
-        expectedClass: '.spinner-double-bounce',
-      },
-      {
-        animation: ngxLoadingAnimationTypes.none,
-        expectedClass: '.backdrop',
-      },
-      {
-        animation: ngxLoadingAnimationTypes.pulse,
-        expectedClass: '.spinner-pulse',
-      },
-      {
-        animation: ngxLoadingAnimationTypes.rectangleBounce,
-        expectedClass: '.spinner-rectangle-bounce',
-      },
-      {
-        animation: ngxLoadingAnimationTypes.rotatingPlane,
-        expectedClass: '.spinner-sk-rotateplane',
-      },
-      {
-        animation: ngxLoadingAnimationTypes.wanderingCubes,
-        expectedClass: '.spinner-wandering-cubes',
-      },
-    ].forEach(({ animation, expectedClass }) => {
-      const config = new NgxLoadingConfig();
-      config.animationType = animation;
-      component.config = config;
-      component.show = true;
-      expect(component.show).toBe(true);
-      fixture.detectChanges();
-      const debugElement: DebugElement = fixture.debugElement;
-      expect(debugElement.query(By.css(expectedClass))).toBeTruthy();
-    });
-  });
+  // it('should show the correct animation when set once show set to true', () => {
+  //   [
+  //     {
+  //       animation: ngxLoadingAnimationTypes.threeBounce,
+  //       expectedClass: '.spinner-three-bounce',
+  //     },
+  //     {
+  //       animation: ngxLoadingAnimationTypes.chasingDots,
+  //       expectedClass: '.spinner-chasing-dots',
+  //     },
+  //     {
+  //       animation: ngxLoadingAnimationTypes.circle,
+  //       expectedClass: '.spinner-circle',
+  //     },
+  //     {
+  //       animation: ngxLoadingAnimationTypes.circleSwish,
+  //       expectedClass: '.spinner-circle-swish',
+  //     },
+  //     {
+  //       animation: ngxLoadingAnimationTypes.cubeGrid,
+  //       expectedClass: '.sk-cube-grid',
+  //     },
+  //     {
+  //       animation: ngxLoadingAnimationTypes.doubleBounce,
+  //       expectedClass: '.spinner-double-bounce',
+  //     },
+  //     {
+  //       animation: ngxLoadingAnimationTypes.none,
+  //       expectedClass: '.backdrop',
+  //     },
+  //     {
+  //       animation: ngxLoadingAnimationTypes.pulse,
+  //       expectedClass: '.spinner-pulse',
+  //     },
+  //     {
+  //       animation: ngxLoadingAnimationTypes.rectangleBounce,
+  //       expectedClass: '.spinner-rectangle-bounce',
+  //     },
+  //     {
+  //       animation: ngxLoadingAnimationTypes.rotatingPlane,
+  //       expectedClass: '.spinner-sk-rotateplane',
+  //     },
+  //     {
+  //       animation: ngxLoadingAnimationTypes.wanderingCubes,
+  //       expectedClass: '.spinner-wandering-cubes',
+  //     },
+  //   ].forEach(({ animation, expectedClass }) => {
+  //     const config = new NgxLoadingConfig();
+  //     config.animationType = animation;
+  //     component.config = config;
+  //     component.show = true;
+  //     expect(component.show).toBe(true);
+  //     fixture.detectChanges();
+  //     const debugElement: DebugElement = fixture.debugElement;
+  //     expect(debugElement.query(By.css(expectedClass))).toBeTruthy();
+  //   });
+  // });
 
-  it('should', () => {
-    const ngxLoadingService = TestBed.inject(NgxLoadingService);
-    const ngxLoadingConfig = new NgxLoadingConfig();
-    ngxLoadingConfig.animationType = ngxLoadingAnimationTypes.threeBounce;
-    ngxLoadingConfig.backdropBackgroundColour = '#ffffff';
-    ngxLoadingConfig.backdropBorderRadius = '1px';
-    ngxLoadingConfig.fullScreenBackdrop = true;
-    ngxLoadingConfig.primaryColour = '#00ffff';
-    ngxLoadingConfig.secondaryColour = '#ff00ff';
-    ngxLoadingConfig.tertiaryColour = '#ffff00';
+  // it('should', () => {
+  //   const ngxLoadingService = TestBed.inject(NgxLoadingService);
+  //   const ngxLoadingConfig = new NgxLoadingConfig();
+  //   ngxLoadingConfig.animationType = ngxLoadingAnimationTypes.threeBounce;
+  //   ngxLoadingConfig.backdropBackgroundColour = '#ffffff';
+  //   ngxLoadingConfig.backdropBorderRadius = '1px';
+  //   ngxLoadingConfig.fullScreenBackdrop = true;
+  //   ngxLoadingConfig.primaryColour = '#00ffff';
+  //   ngxLoadingConfig.secondaryColour = '#ff00ff';
+  //   ngxLoadingConfig.tertiaryColour = '#ffff00';
 
-    ngxLoadingService.loadingConfig = ngxLoadingConfig;
+  //   ngxLoadingService.loadingConfig = ngxLoadingConfig;
 
-    fixture = TestBed.createComponent(NgxLoadingComponent);
-    component = fixture.componentInstance;
-    component.ngOnInit();
-    fixture.detectChanges();
+  //   fixture = TestBed.createComponent(NgxLoadingComponent);
+  //   component = fixture.componentInstance;
+  //   component.ngOnInit();
+  //   fixture.detectChanges();
 
-    component.show = true;
-    expect(component.show).toBe(true);
-    fixture.detectChanges();
-    const debugElement: DebugElement = fixture.debugElement;
-    expect(debugElement.query(By.css('.spinner-three-bounce'))).toBeTruthy();
-  });
+  //   component.show = true;
+  //   expect(component.show).toBe(true);
+  //   fixture.detectChanges();
+  //   const debugElement: DebugElement = fixture.debugElement;
+  //   expect(debugElement.query(By.css('.spinner-three-bounce'))).toBeTruthy();
+  // });
 });
