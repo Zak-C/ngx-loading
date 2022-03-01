b42650ebc6bd87381c2e2794e056f2762594b304
Zak Cox <zakkcoxx@gmail.com>
2022-03-01 16:28:37 +0000

    Added unit tests.

--- projects/ngx-loading/src/lib/ngx-loading.component.spec.ts
+++ projects/ngx-loading/src/lib/ngx-loading.component.spec.ts
@@ -120,28 +120,96 @@ describe('NgxLoadingComponent', () => {
     });
   });
 
-  it('should', () => {
-    const ngxLoadingService = TestBed.inject(NgxLoadingService);
-    const ngxLoadingConfig = new NgxLoadingConfig();
-    ngxLoadingConfig.animationType = ngxLoadingAnimationTypes.threeBounce;
-    ngxLoadingConfig.backdropBackgroundColour = '#ffffff';
-    ngxLoadingConfig.backdropBorderRadius = '1px';
-    ngxLoadingConfig.fullScreenBackdrop = true;
-    ngxLoadingConfig.primaryColour = '#00ffff';
-    ngxLoadingConfig.secondaryColour = '#ff00ff';
-    ngxLoadingConfig.tertiaryColour = '#ffff00';
+  it('should show the correct animation when set at the service level and show set to true', () => {
+    [
+      {
+        animation: ngxLoadingAnimationTypes.threeBounce,
+        expectedClass: '.spinner-three-bounce',
+      },
+      {
+        animation: ngxLoadingAnimationTypes.chasingDots,
+        expectedClass: '.spinner-chasing-dots',
+      },
+      {
+        animation: ngxLoadingAnimationTypes.circle,
+        expectedClass: '.spinner-circle',
+      },
+      {
+        animation: ngxLoadingAnimationTypes.circleSwish,
+        expectedClass: '.spinner-circle-swish',
+      },
+      {
+        animation: ngxLoadingAnimationTypes.cubeGrid,
+        expectedClass: '.sk-cube-grid',
+      },
+      {
+        animation: ngxLoadingAnimationTypes.doubleBounce,
+        expectedClass: '.spinner-double-bounce',
+      },
+      {
+        animation: ngxLoadingAnimationTypes.none,
+        expectedClass: '.backdrop',
+      },
+      {
+        animation: ngxLoadingAnimationTypes.pulse,
+        expectedClass: '.spinner-pulse',
+      },
+      {
+        animation: ngxLoadingAnimationTypes.rectangleBounce,
+        expectedClass: '.spinner-rectangle-bounce',
+      },
+      {
+        animation: ngxLoadingAnimationTypes.rotatingPlane,
+        expectedClass: '.spinner-sk-rotateplane',
+      },
+      {
+        animation: ngxLoadingAnimationTypes.wanderingCubes,
+        expectedClass: '.spinner-wandering-cubes',
+      },
+    ].forEach(({ animation, expectedClass }) => {
+      const ngxLoadingService = TestBed.inject(NgxLoadingService);
+      const ngxLoadingConfig = new NgxLoadingConfig();
+      ngxLoadingConfig.animationType = animation;
+      ngxLoadingConfig.backdropBackgroundColour = '#ffffff';
+      ngxLoadingConfig.backdropBorderRadius = '1px';
+      ngxLoadingConfig.fullScreenBackdrop = true;
+      ngxLoadingConfig.primaryColour = '#00ffff';
+      ngxLoadingConfig.secondaryColour = '#ff00ff';
+      ngxLoadingConfig.tertiaryColour = '#ffff00';
 
-    ngxLoadingService.loadingConfig = ngxLoadingConfig;
+      ngxLoadingService.loadingConfig = ngxLoadingConfig;
 
-    fixture = TestBed.createComponent(NgxLoadingComponent);
-    component = fixture.componentInstance;
-    component.ngOnInit();
-    fixture.detectChanges();
+      fixture = TestBed.createComponent(NgxLoadingComponent);
+      component = fixture.componentInstance;
+      component.ngOnInit();
+      fixture.detectChanges();
 
-    component.show = true;
-    expect(component.show).toBe(true);
-    fixture.detectChanges();
-    const debugElement: DebugElement = fixture.debugElement;
-    expect(debugElement.query(By.css('.spinner-three-bounce'))).toBeTruthy();
+      component.show = true;
+      expect(component.show).toBe(true);
+      fixture.detectChanges();
+      const debugElement: DebugElement = fixture.debugElement;
+      expect(component.config.animationType).toEqual(
+        ngxLoadingConfig.animationType
+      );
+      expect(component.config.backdropBackgroundColour).toEqual(
+        ngxLoadingConfig.backdropBackgroundColour
+      );
+      expect(component.config.backdropBorderRadius).toEqual(
+        ngxLoadingConfig.backdropBorderRadius
+      );
+      expect(component.config.fullScreenBackdrop).toEqual(
+        ngxLoadingConfig.fullScreenBackdrop
+      );
+      expect(component.config.primaryColour).toEqual(
+        ngxLoadingConfig.primaryColour
+      );
+      expect(component.config.secondaryColour).toEqual(
+        ngxLoadingConfig.secondaryColour
+      );
+      expect(component.config.tertiaryColour).toEqual(
+        ngxLoadingConfig.tertiaryColour
+      );
+      expect(debugElement.query(By.css(expectedClass))).toBeTruthy();
+    });
   });
 });
